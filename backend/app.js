8935594b2827fac1333c1511adb095cd88304a9a
Vivek Sharma <viveksharmaa252@gmail.com>
2026-02-20 10:47:50 +0530

    Creating CRUD routes

--- backend/app.js
+++ backend/app.js
@@ -81,8 +81,51 @@ app.post("/listings", async (req, res) => {
     console.log("Error saving new listing:", err);
     res.status(500).json({ message: "Error saving new listing" });
   }
-
   res.send("Data received successfully");
+});
+
+// update route
+app.patch("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: "Invalid ID format" });
+  }
+  const updatedListing = req.body.listing;
+  Object.keys(updatedListing).forEach((key) => {
+    if(updatedListing[key] === undefined || updatedListing[key].toString().trim() === "") {
+      delete updatedListing[key];
+    }
+  })
+  try {
+    const updated = await Listing.findByIdAndUpdate(id, {$set: updatedListing}, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    if (!updated) {
+      return res.status(404).json({ message: "Listing not found" });
+    }
+    console.log("Updated listing:", updated);
+    res.status(200).json(updated);
+  } catch (err) {
+    res.status(500).json({ message: "Error updating listing" });
+  }
+});
+
+app.delete("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: "Invalid ID format" });
+  }
+  try {
+    const deleted = await Listing.findByIdAndDelete(id);
+    if (!deleted) {
+      return res.status(404).json({ message: "Listing not found" });
+    }
+    console.log("Deleted listing:", deleted);
+    res.status(200).json(deleted);
+  } catch (err) {
+    res.status(500).json({ message: "Error deleting listing" });
+  }
 });
 
 app.listen(PORT, () => {
--- backend/models/listing.js
+++ backend/models/listing.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
 const listingSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  title: { type: String, required: true, trim: true },
+  description: { type: String, required: true, trim: true },
   price: { type: Number, required: true },
-  location: { type: String, required: true },
+  location: { type: String, required: true, trim: true },
   image: {
     default:
       "https://images.unsplash.com/photo-1501785888041-af3ef285b470?q=80&w=2070&auto=format&fit=crop",
     type: String,
+    trim: true,
     set: (v) => {
       if (typeof v !== "string" || v.trim() === "") {
         return "https://via.placeholder.com/150";
@@ -16,7 +17,7 @@ const listingSchema = new mongoose.Schema({
       return v;
     },
   },
-  country: { type: String, required: true },
+  country: { type: String, required: true, trim: true  },
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
